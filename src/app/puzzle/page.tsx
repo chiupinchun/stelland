8b76157302b4bac9ca9ac299fa333f5dce665b4e
Hato <fansintuesan@gmail.com>
2024-08-25 23:11:34 +0800

    feat: puzzle game can check complete

--- src/app/puzzle/page.tsx
+++ src/app/puzzle/page.tsx
@@ -1,4 +1,4 @@
-import { FC, useEffect, useState } from 'react'
+import { FC, useCallback, useEffect, useState } from 'react'
 import SpiritsPicture from '@/assets/images/spirits.webp'
 
 const PIECE_COUNT_X = 4
@@ -11,6 +11,15 @@ interface PuzzlePiece {
   x: number
   y: number
   isEmpty: boolean
+}
+
+const Piece: FC<PuzzlePiece & { onClick: () => void }> = ({ onClick, ...piece }) => {
+  return <div onClick={onClick} className='w-5 h-5 border cursor-pointer' style={{
+    width: PIECE_WIDTH + 'px',
+    height: PIECE_HEIGHT + 'px',
+    backgroundImage: piece.isEmpty ? '' : `url(${SpiritsPicture})`,
+    backgroundPosition: `${piece.x * 33}% ${piece.y * 50}%`
+  }} />
 }
 
 interface Props { }
@@ -42,6 +51,40 @@ const Puzzle: FC<Props> = () => {
 
   useEffect(resetPieces, [])
 
+  const [selectedPice, setSelectedPiece] = useState<PuzzlePiece | null>(null)
+
+  const handleSelectPiece = (piece: PuzzlePiece) => {
+    if (selectedPice) {
+      switchPieces(piece, selectedPice)
+    } else {
+      setSelectedPiece(piece)
+    }
+  }
+
+  const switchPieces = (piece1: PuzzlePiece, piece2: PuzzlePiece) => {
+    const tempPiece = { ...piece1 }
+    Object.assign(piece1, piece2)
+    Object.assign(piece2, tempPiece)
+    setPieces([...pieces])
+    setSlots([...slots])
+    setSelectedPiece(null)
+
+    const isComplete = checkComplete()
+    if (isComplete) {
+      alert('過關！')
+    }
+  }
+
+  const checkComplete = () => {
+    for (let i = 0; i < PIECE_COUNT_X * PIECE_COUNT_y; i++) {
+      const currentSlot = slots[i]
+      if (currentSlot.isEmpty || currentSlot.id !== i + 1) {
+        return false
+      }
+    }
+    return true
+  }
+
   return (
     <>
       <div className='flex justify-center items-center gap-10 h-screen'>
@@ -49,13 +92,8 @@ const Puzzle: FC<Props> = () => {
           <h2>插槽區</h2>
           <ul className='grid grid-cols-4 gap-2'>
             {slots.map(piece => (
-              <li key={piece.id}>
-                <div className='w-5 h-5 border' style={{
-                  width: PIECE_WIDTH + 'px',
-                  height: PIECE_HEIGHT + 'px',
-                  backgroundImage: piece.isEmpty ? '' : `url(${SpiritsPicture})`,
-                  backgroundPosition: `${piece.x * 33}% ${piece.y * 50}%`
-                }} />
+              <li key={piece.id + '' + piece.isEmpty} className={selectedPice === piece ? 'shadow-2xl shadow-blue-300' : ''}>
+                <Piece onClick={() => handleSelectPiece(piece)} {...piece} />
               </li>
             ))}
           </ul>
@@ -65,13 +103,8 @@ const Puzzle: FC<Props> = () => {
           <h2>碎片區</h2>
           <ul className='grid grid-cols-2 gap-2'>
             {pieces.map(piece => (
-              <li key={piece.id}>
-                <div className='w-5 h-5 border' style={{
-                  width: PIECE_WIDTH + 'px',
-                  height: PIECE_HEIGHT + 'px',
-                  backgroundImage: `url(${SpiritsPicture})`,
-                  backgroundPosition: `${piece.x * 33}% ${piece.y * 50}%`
-                }} />
+              <li key={piece.id + '' + piece.isEmpty} className={selectedPice === piece ? 'shadow-2xl shadow-blue-300' : ''}>
+                <Piece onClick={() => handleSelectPiece(piece)} {...piece} />
               </li>
             ))}
           </ul>
