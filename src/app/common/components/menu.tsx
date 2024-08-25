f0b1f49a405d28f2ea5c15476f19d5e314ed14ab
Hato <fansintuesan@gmail.com>
2024-08-25 22:03:31 +0800

    style: set menu position to screen-right

--- src/app/common/components/menu.tsx
+++ src/app/common/components/menu.tsx
@@ -49,50 +49,52 @@ const Menu: FC<Props> = () => {
 
   return (
     <>
-      <nav className={
-        'fixed top-5 z-10 flex justify-center w-screen transition-all '
-        + (isMenuOpen ? 'right-0 opacity-100' : '-right-[100vw] opacity-0')
-      }>
-        <ul className='flex items-start gap-10 lg:gap-20'>
-          {menu.map(group => (
-            <li
-              onClick={(e) => {
-                e.stopPropagation()
-                setActiveGroup(group)
-              }}
-              onMouseEnter={() => setActiveGroup(group)}
-              onMouseLeave={clearActiveGroup} key={group.title}
-            >
-              {
-                group.href
-                  ? <Link to={group.href} className='block meteor'>{group.title}</Link>
-                  : <a className='flex items-center gap-2 cursor-pointer meteor'>
-                    {group.title}
-                    <ChevronDown className={'transition ' + (isGroupActive(group) ? 'rotate-180' : 'rotate-0')} width={20} height={20} />
-                  </a>
-              }
-              {
-                group.children && <ul className={
-                  'ps-1 overflow-hidden transition-all ' + (isGroupActive(group) ? 'max-h-screen' : 'max-h-0')
-                }>
-                  {group.children.map(link => (
-                    <li key={link.title}>
-                      <Link to={link.href} className='block meteor'>{link.title}</Link>
-                    </li>
-                  ))}
-                </ul>
-              }
-            </li>
-          ))}
-        </ul>
-      </nav>
+      <div className='fixed right-5 top-5 z-10'>
+        <nav className={
+          'absolute flex justify-center w-96 transition-all '
+          + (isMenuOpen ? 'right-5 lg:right-16 opacity-100' : '-right-96 opacity-0')
+        }>
+          <ul className='flex items-start gap-10 lg:gap-20'>
+            {menu.map(group => (
+              <li
+                onClick={(e) => {
+                  e.stopPropagation()
+                  setActiveGroup(group)
+                }}
+                onMouseEnter={() => setActiveGroup(group)}
+                onMouseLeave={clearActiveGroup} key={group.title}
+              >
+                {
+                  group.href
+                    ? <Link to={group.href} className='block meteor'>{group.title}</Link>
+                    : <a className='flex items-center gap-2 cursor-pointer meteor'>
+                      {group.title}
+                      <ChevronDown className={'transition ' + (isGroupActive(group) ? 'rotate-180' : 'rotate-0')} width={20} height={20} />
+                    </a>
+                }
+                {
+                  group.children && <ul className={
+                    'ps-1 overflow-hidden transition-all ' + (isGroupActive(group) ? 'max-h-screen' : 'max-h-0')
+                  }>
+                    {group.children.map(link => (
+                      <li key={link.title}>
+                        <Link to={link.href} className='block meteor'>{link.title}</Link>
+                      </li>
+                    ))}
+                  </ul>
+                }
+              </li>
+            ))}
+          </ul>
+        </nav>
 
-      <button
-        onClick={() => setIsMenuOpen(!isMenuOpen)}
-        className='fixed right-5 top-5 z-10 p-2 bg-slate-300 rounded-lg text-slate-700'
-      >
-        <AlignJustify />
-      </button>
+        <button
+          onClick={() => setIsMenuOpen(!isMenuOpen)}
+          className='p-2 bg-slate-300 rounded-lg text-slate-700'
+        >
+          <AlignJustify />
+        </button>
+      </div>
     </>
   )
 }
