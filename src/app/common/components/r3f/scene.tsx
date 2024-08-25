0e844ee97f19e63eec0dc7449dfaa3b18e03d6ac
Hato <fansintuesan@gmail.com>
2024-08-25 17:24:48 +0800

    feat: lignt indensity will become dark at night

--- src/app/common/components/r3f/scene.tsx
+++ src/app/common/components/r3f/scene.tsx
@@ -1,5 +1,5 @@
 import { Canvas } from '@react-three/fiber'
-import { FC } from 'react'
+import { FC, useRef } from 'react'
 import { CAMERA_OFFSET } from '@/app/common/constants/ui-model'
 
 interface Props {
@@ -7,6 +7,7 @@ interface Props {
 }
 
 const Scene: FC<Props> = ({ children }) => {
+  const lightIntensity = useRef(0.7 - 0.5 * Math.abs(new Date().getHours() - 12) / 12)
 
   return (
     <>
@@ -16,7 +17,7 @@ const Scene: FC<Props> = ({ children }) => {
         <directionalLight intensity={0.7} castShadow shadow-bias={-0.0004} position={[-20, 20, 20]}>
           <orthographicCamera attach="shadow-camera" args={[-20, 20, 20, -20]} />
         </directionalLight>
-        <ambientLight intensity={0.2} />
+        <ambientLight intensity={lightIntensity.current} />
         {children}
       </Canvas>
     </>
