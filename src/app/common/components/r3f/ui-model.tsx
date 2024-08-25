34429e47abbe5f3940bc945ff601c2a36903697b
Hato <fansintuesan@gmail.com>
2024-08-25 10:34:27 +0800

    feat: load models

--- src/app/common/components/r3f/ui-model.tsx
+++ src/app/common/components/r3f/ui-model.tsx
@@ -2,18 +2,20 @@ import { FC, useEffect, useRef } from 'react'
 import { useLoader } from '@react-three/fiber'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
 import { Mesh } from 'three'
+import { Coordinate3d } from '@/app/common/types/math'
 
 interface Props {
   src: string
+  rawPosition?: Coordinate3d
 }
 
-const UiModel: FC<Props> = ({ src }) => {
+const UiModel: FC<Props> = ({ src, rawPosition = [0, 0, 0] }) => {
   const model = useLoader(GLTFLoader, src)
   const modelRef = useRef<React.MutableRefObject<Mesh>>(null!)
 
   return (
     <>
-      <primitive object={model.scene} ref={modelRef} />
+      <primitive object={model.scene} ref={modelRef} position={rawPosition} />
     </>
   )
 }
--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,9 +1,14 @@
 import { FC } from 'react'
+import Cat from '@/assets/models/cat.glb?url'
+import Chikun from '@/assets/models/chikun.glb?url'
+import Dog from '@/assets/models/dog.glb?url'
+import Fish from '@/assets/models/fish.glb?url'
 import PrairieDogGlb from '@/assets/models/prairie-dog.glb?url'
+import Snake from '@/assets/models/snake.glb?url'
 import UiModel from '@/app/common/components/r3f/ui-model'
 import Scene from '@/app/common/components/r3f/scene'
 import Ground from '@/app/common/components/r3f/ground'
-import StarNight from '../common/components/r3f/star-night'
+import StarNight from '@/app/common/components/r3f/star-night'
 
 interface Props { }
 
@@ -15,7 +20,12 @@ const HomePage: FC<Props> = () => {
         <Scene>
           <Ground />
           <StarNight count={100} />
-          <UiModel src={PrairieDogGlb} />
+          <UiModel src={Fish} rawPosition={[-1, 0, 0]} />
+          <UiModel src={Cat} rawPosition={[0, 0, 0]} />
+          <UiModel src={Dog} rawPosition={[1, 0, 0]} />
+          <UiModel src={Chikun} rawPosition={[0, 0, -1]} />
+          <UiModel src={PrairieDogGlb} rawPosition={[1, 0, -1]} />
+          <UiModel src={Snake} rawPosition={[-1, 0, -1]} />
         </Scene>
       </div>
     </>
