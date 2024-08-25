1f06d5de6ee847496e4f9551205a6934f74525c8
Hato <fansintuesan@gmail.com>
2024-08-25 09:21:25 +0800

    chore: add r3f and assets

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
+import { Canvas } from '@react-three/fiber'
 import './App.css'
+import { useLoader } from '@react-three/fiber'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
+import PrairieDogGlb from './assets/models/prairie-dog.glb?url'
 
 function App() {
+  const model = useLoader(GLTFLoader, PrairieDogGlb)
 
   return (
     <>
       <div className='border'>
-        test
+        <Canvas>
+          <primitive object={model.scene} />
+        </Canvas>
       </div>
     </>
   )
