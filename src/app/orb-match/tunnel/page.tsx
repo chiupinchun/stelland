f97399b4d5466eb79cda91f7a2009bf2c5d39fb9
Hato <fansintuesan@gmail.com>
2024-08-31 19:57:45 +0800

    feat: tunnel flow

--- src/app/orb-match/tunnel/page.tsx
+++ src/app/orb-match/tunnel/page.tsx
@@ -2,22 +2,28 @@ import React, { useState } from 'react';
 import Scene from '@/app/common/components/r3f/scene';
 import Tunnel from './components/tunnel';
 import Camera from './components/camera';
+import { Link } from 'react-router-dom';
 
-const WALK_DURATION = 2500
+const WALK_DURATION = 2000
+const STAGE_COUNT = 3
 
 const TunnelPage: React.FC = () => {
   const [isWalking, setIsWalking] = useState(false)
-  const [isStarted, setIsStarted] = useState(false)
+  const [stage, setStage] = useState(0)
 
-  const handleStart = () => {
-    setIsStarted(true)
+  const nextStage = () => {
     setIsWalking(true)
+    setStage(stage + 1)
 
     setTimeout(() => {
       setIsWalking(false)
     }, WALK_DURATION)
   }
 
+  const handleSelectBonus = (bonus: number) => {
+    console.log(bonus)
+    nextStage()
+  }
 
   return (
     <div className="h-screen">
@@ -38,8 +44,22 @@ const TunnelPage: React.FC = () => {
         <Tunnel />
       </Scene>
 
-      <div className='fixed top-0 w-screen h-screen flex justify-center items-center'>
-        {isStarted || <button onClick={handleStart} className='px-5 py-2 rounded-xl bg-slate-300 hover:bg-slate-100'>開始試煉</button>}
+      <div className={
+        'fixed top-0 w-screen h-screen flex justify-center items-center ' + (
+          !isWalking && (stage === 0 || stage === STAGE_COUNT) ? 'bg-slate-900 bg-opacity-75' : ''
+        )
+      }>
+        {stage === 0 && <button onClick={nextStage} className='px-5 py-2 rounded-xl bg-slate-300 hover:bg-slate-100'>開始試煉</button>}
+        {stage > 0 && stage < STAGE_COUNT && !isWalking ? <div className='flex gap-40'>
+          {[1, 2, 3].map((num) => (
+            <div onClick={() => handleSelectBonus(num)} className='w-48 h-96 bg-slate-300 rounded-xl cursor-pointer' key={num}>
+              祝福{num}
+            </div>
+          ))}
+        </div> : null}
+        {stage >= STAGE_COUNT && !isWalking ? <Link to='/orb-match/battle' className='px-5 py-2 rounded-xl bg-slate-300 hover:bg-slate-100'>
+          謁見星靈
+        </Link> : null}
       </div>
     </div>
   );
