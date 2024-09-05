17abd26bddc74d418f1ec61cf6f3f6db7d8c4d74
Hato <fansintuesan@gmail.com>
2024-09-05 20:51:47 +0800

    refactor: seperate select event logic to component

---
+++ src/app/orb-match/tunnel/components/eventSelector.tsx
@@ -0,0 +1,75 @@
+import { useMemo, useState } from "react"
+import { Event, legendEvents, normalEvents, rareEvents } from "@/app/orb-match/core/events"
+import { getRandomInt } from "@/app/common/utils/math"
+import { LEGEND_EVENT_RATE, RARE_EVENT_RATE } from "../constants/rate"
+
+const EventSelector: React.FC<{
+  stage: number,
+  onSelect: () => void
+}> = ({ stage, onSelect }) => {
+  const events = useMemo(() => {
+    const pickupEvents: Event[] = []
+
+    const _normalEvents = [...normalEvents]
+    const _rareEvents = [...rareEvents]
+    const _legentEvents = [...legendEvents]
+
+    const spliceEvent = (events: Event[]) => {
+      const [event] = events.splice(getRandomInt(0, events.length), 1)
+      return event
+    }
+
+    const pickEvent = () => {
+      let random = Math.random()
+      if (random < LEGEND_EVENT_RATE) { return spliceEvent(_legentEvents) }
+      if (random - LEGEND_EVENT_RATE < RARE_EVENT_RATE) { return spliceEvent(_rareEvents) }
+      return spliceEvent(_normalEvents)
+    }
+
+    const isBonus = stage % 3 === 0
+    if (isBonus) {
+      const legendEvent = spliceEvent(_legentEvents)
+      pickupEvents.push(legendEvent)
+    }
+
+    while (pickupEvents.length < 3) {
+      const event = pickEvent()
+      pickupEvents.push(event)
+    }
+
+    return pickupEvents
+  }, [stage])
+
+  const [selectedEvent, setSelectedEvent] = useState<null | Event>(null)
+
+  const confirmEvent = () => {
+    if (!selectedEvent) { return }
+
+    setSelectedEvent(null)
+    onSelect()
+  }
+
+  return (<>
+    <div className='flex gap-40'>
+      {events.map((event) => (
+        <div
+          onClick={() => setSelectedEvent(event)}
+          className={'p-5 w-52 h-72 bg-slate-300 rounded-xl cursor-pointer ' + (
+            selectedEvent === event ? 'shadow-2xl shadow-blue-500' : ''
+          )}
+          key={event.id}
+        >
+          <h2 className='font-bold text-lg'>{event.name}</h2>
+          <p className='my-2'>{event.description}</p>
+        </div>
+      ))}
+    </div>
+    <button
+      onClick={confirmEvent}
+      disabled={selectedEvent === null}
+      className='mt-16 px-12 py-2 rounded bg-blue-200 hover:bg-blue-100 disabled:bg-gray-400 cursor-pointer disabled:cursor-not-allowed'
+    >確認</button>
+  </>)
+}
+
+export default EventSelector
--- src/app/orb-match/tunnel/page.tsx
+++ src/app/orb-match/tunnel/page.tsx
@@ -1,15 +1,13 @@
-import React, { useEffect, useMemo, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Scene from '@/app/common/components/r3f/scene';
 import Tunnel from './components/tunnel';
 import Camera from './components/camera';
 import { Link } from 'react-router-dom';
 import { createUser, getUserInfo, UserInfoResponse } from '@/api/module/orb-match';
-import { Event, legendEvents, normalEvents, rareEvents } from '../core/events';
-import { getRandomInt } from '@/app/common/utils/math';
-import { LEGEND_EVENT_RATE, RARE_EVENT_RATE } from './constants/rate';
+import EventSelector from './components/eventSelector';
 
 const WALK_DURATION = 2000
-const STAGE_COUNT = 3
+const STAGE_COUNT = 10
 
 const TunnelPage: React.FC = () => {
   const [isWalking, setIsWalking] = useState(false)
@@ -29,41 +27,8 @@ const TunnelPage: React.FC = () => {
     }, WALK_DURATION)
   }
 
-  const events = useMemo(() => {
-    const pickupEvents: Event[] = []
-
-    const _normalEvents = [...normalEvents]
-    const _rareEvents = [...rareEvents]
-    const _legentEvents = [...legendEvents]
-
-    const spliceEvent = (events: Event[]) => {
-      const [event] = events.splice(getRandomInt(0, events.length), 1)
-      return event
-    }
-
-    const pickEvent = () => {
-      let random = Math.random()
-      if (random < LEGEND_EVENT_RATE) { return spliceEvent(_legentEvents) }
-      if (random - LEGEND_EVENT_RATE < RARE_EVENT_RATE) { return spliceEvent(_rareEvents) }
-      return spliceEvent(_normalEvents)
-    }
-
-    const isBonus = stage % 3 === 2
-    if (isBonus) {
-      const legendEvent = spliceEvent(_legentEvents)
-      events.push(legendEvent)
-    }
-
-    while (pickupEvents.length < 3) {
-      const event = pickEvent()
-      pickupEvents.push(event)
-    }
-
-    return pickupEvents
-  }, [stage])
-
-  const handleSelectBonus = (event: Event) => {
-    console.log(event)
+  const handleSelectEvent = () => {
+    // TODO: post event
     nextStage()
   }
 
@@ -96,18 +61,14 @@ const TunnelPage: React.FC = () => {
       </Scene>
 
       <div className={
-        'fixed top-0 w-screen h-screen flex justify-center items-center ' + (
+        'fixed top-0 w-screen h-screen flex flex-col justify-center items-center ' + (
           !isWalking && (stage === 0 || stage === STAGE_COUNT) ? 'bg-slate-900 bg-opacity-75' : ''
         )
       }>
         {stage === 0 && <button onClick={nextStage} className='px-5 py-2 rounded-xl bg-slate-300 hover:bg-slate-100'>開始試煉</button>}
-        {stage > 0 && stage < STAGE_COUNT && !isWalking ? <div className='flex gap-40'>
-          {events.map((event) => (
-            <div onClick={() => handleSelectBonus(event)} className='w-48 h-96 bg-slate-300 rounded-xl cursor-pointer' key={event.id}>
-              祝福 {event.name}
-            </div>
-          ))}
-        </div> : null}
+        {stage > 0 && stage < STAGE_COUNT && !isWalking
+          ? <EventSelector onSelect={handleSelectEvent} stage={stage} />
+          : null}
         {stage >= STAGE_COUNT && !isWalking ? <Link to='/orb-match/battle' className='px-5 py-2 rounded-xl bg-slate-300 hover:bg-slate-100'>
           謁見星靈
         </Link> : null}
