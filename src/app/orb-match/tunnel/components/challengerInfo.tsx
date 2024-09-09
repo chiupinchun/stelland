6bfe2318a43fe2948f9d650f60ae1742fe3b4989
Hato <fansintuesan@gmail.com>
2024-09-09 20:53:16 +0800

    feat: challenger info component can switch tab

--- src/app/orb-match/tunnel/components/challengerInfo.tsx
+++ src/app/orb-match/tunnel/components/challengerInfo.tsx
@@ -1,26 +1,31 @@
 import { UserInfoResponse } from '@/api/module/orb-match'
 import { X } from 'lucide-react'
-import { FC } from 'react'
+import { FC, useRef, useState } from 'react'
 import { Challenger } from '@/app/orb-match/core/challenger'
-import { getWeaponById } from '../../core/weapons'
-import { getBlessingById } from '../../core/blessings'
+import { getWeaponById, Weapon } from '@/app/orb-match/core/weapons'
+import { getBlessingById } from '@/app/orb-match/core/blessings'
 import Card from '@/app/common/components/ui/card'
+import { label } from 'three/webgpu'
 
 interface TableItem {
   label: string
   cell: string | number
 }
 
-interface Props {
-  user: UserInfoResponse
-  onClose: () => void
-}
-
-const ChallengerInfo: FC<Props> = ({ user, onClose }) => {
-  const challenger = new Challenger(
-    user.weapon ? getWeaponById(user.weapon) : null,
-    user.blessings.map(id => getBlessingById(id))
-  )
+const BaseInfo: FC<{
+  weapon: Weapon | null
+  challenger: Challenger
+}> = ({ weapon, challenger }) => {
+  const weaponInfo: TableItem[] = [
+    {
+      label: '武器',
+      cell: weapon?.name ?? '無'
+    },
+    {
+      label: '效果',
+      cell: weapon?.description ?? ''
+    }
+  ]
 
   const baseInfo: TableItem[] = [
     {
@@ -108,65 +113,121 @@ const ChallengerInfo: FC<Props> = ({ user, onClose }) => {
       cell: `${challenger.healRate}%`
     }
   ]
+  return (
+    <>
+      <h2 className='mb-1 border-b border-slate-400 font-bold'>武器資訊</h2>
+      <ul className='pe-4'>
+        {weaponInfo.map((item, index) => (
+          <li key={index} className='flex justify-between border-b border-slate-300 hover:border-slate-200'>
+            <div>{item.label}</div>
+            <div className='max-w-[50%]'>{item.cell}</div>
+          </li>
+        ))}
+      </ul>
+
+      <h2 className='mb-1 border-b border-slate-400 font-bold'>能力值</h2>
+      <ul className='pe-4'>
+        {baseInfo.map((item, index) => (
+          <li key={index} className='flex justify-between border-b border-slate-300 hover:border-slate-200'>
+            <div>{item.label}</div>
+            <div>{item.cell}</div>
+          </li>
+        ))}
+      </ul>
+
+      <h2 className='mt-5 mb-1 border-b border-slate-400 font-bold'>暴擊相關</h2>
+      <ul className='pe-4'>
+        {criticalInfo.map((item, index) => (
+          <li key={index} className='flex justify-between border-b border-slate-300 hover:border-slate-200'>
+            <div>{item.label}</div>
+            <div>{item.cell}</div>
+          </li>
+        ))}
+      </ul>
+
+      <h2 className='mt-5 mb-1 border-b border-slate-400 font-bold'>屬性傷害</h2>
+      <ul className='pe-4'>
+        {elementDmgInfo.map((item, index) => (
+          <li key={index} className='flex justify-between border-b border-slate-300 hover:border-slate-200'>
+            <div>{item.label}</div>
+            <div>{item.cell}</div>
+          </li>
+        ))}
+      </ul>
+
+      <h2 className='mt-5 mb-1 border-b border-slate-400 font-bold'>屬性抗性</h2>
+      <ul className='pe-4'>
+        {registanceInfo.map((item, index) => (
+          <li key={index} className='flex justify-between border-b border-slate-300 hover:border-slate-200'>
+            <div>{item.label}</div>
+            <div>{item.cell}</div>
+          </li>
+        ))}
+      </ul>
+
+      <h2 className='mt-5 mb-1 border-b border-slate-400 font-bold'>其他資訊</h2>
+      <ul className='pe-4'>
+        {otherInfo.map((item, index) => (
+          <li key={index} className='flex justify-between border-b border-slate-300 hover:border-slate-200'>
+            <div>{item.label}</div>
+            <div>{item.cell}</div>
+          </li>
+        ))}
+      </ul>
+    </>
+  )
+}
+
+interface Props {
+  user: UserInfoResponse
+  onClose: () => void
+}
+
+const ChallengerInfo: FC<Props> = ({ user, onClose }) => {
+  const tabs = useRef([
+    {
+      label: '基本資訊'
+    },
+    {
+      label: '道具'
+    },
+    {
+      label: '祝福'
+    }
+  ])
+  const [currentTab, setCurrentTab] = useState(tabs.current[0])
+
+  const weapon = user.weapon ? getWeaponById(user.weapon) : null
+  const challenger = new Challenger(
+    weapon,
+    user.blessings.map(id => getBlessingById(id))
+  )
 
   return (
     <>
       <Card onClick={e => e.stopPropagation()} className='bg-slate-300 rounded'>
-        <div className='flex justify-end'>
+        <div className='flex justify-between'>
+          <ul className='flex items-end w-full h-10 border-b'>
+            {tabs.current.map(tab => (
+              <li
+                key={tab.label}
+                onClick={() => setCurrentTab(tab)}
+                className={
+                  'border rounded-tl-lg rounded-tr-lg cursor-pointer transition-all ' + (
+                    tab === currentTab ? 'px-4 py-2' : 'px-2 py-1'
+                  )
+                }
+              >
+                {tab.label}
+              </li>
+            ))}
+          </ul>
           <X onClick={onClose} className='cursor-pointer' />
         </div>
+
         <div className='w-64 lg:w-96 h-[75vh] overflow-y-auto scroll-bar'>
-          <h2 className='mb-1 border-b border-slate-400 font-bold'>基本資訊</h2>
-          <ul>
-            {baseInfo.map((item, index) => (
-              <li key={index} className='flex justify-between'>
-                <div>{item.label}</div>
-                <div>{item.cell}</div>
-              </li>
-            ))}
-          </ul>
-
-          <h2 className='mt-5 mb-1 border-b border-slate-400 font-bold'>暴擊相關</h2>
-          <ul>
-            {criticalInfo.map((item, index) => (
-              <li key={index} className='flex justify-between'>
-                <div>{item.label}</div>
-                <div>{item.cell}</div>
-              </li>
-            ))}
-          </ul>
-
-          <h2 className='mt-5 mb-1 border-b border-slate-400 font-bold'>屬性傷害</h2>
-          <ul>
-            {elementDmgInfo.map((item, index) => (
-              <li key={index} className='flex justify-between'>
-                <div>{item.label}</div>
-                <div>{item.cell}</div>
-              </li>
-            ))}
-          </ul>
-
-          <h2 className='mt-5 mb-1 border-b border-slate-400 font-bold'>屬性抗性</h2>
-          <ul>
-            {registanceInfo.map((item, index) => (
-              <li key={index} className='flex justify-between'>
-                <div>{item.label}</div>
-                <div>{item.cell}</div>
-              </li>
-            ))}
-          </ul>
-
-          <h2 className='mt-5 mb-1 border-b border-slate-400 font-bold'>其他資訊</h2>
-          <ul>
-            {otherInfo.map((item, index) => (
-              <li key={index} className='flex justify-between'>
-                <div>{item.label}</div>
-                <div>{item.cell}</div>
-              </li>
-            ))}
-          </ul>
+          {currentTab === tabs.current[0] ? <BaseInfo challenger={challenger} weapon={weapon} /> : null}
         </div>
-
       </Card>
     </>
   )
