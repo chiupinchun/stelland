c3d99cb91d3750f941e9214294b1f1a4214fa934
Hato <fansintuesan@gmail.com>
2024-09-15 17:06:31 +0800

    feat: challenger hp

--- src/app/orb-match/battle/components/bossArea.tsx
+++ src/app/orb-match/battle/components/bossArea.tsx
@@ -1,27 +1,20 @@
 import Scene from '@/app/common/components/r3f/scene'
 import UiModel from '@/app/common/components/r3f/ui-model'
-import HpBar from '@/app/common/components/ui/hpBar'
 import { FC } from 'react'
-import { Sprite } from '@/app/orb-match/core/sprites'
 
 interface Props {
-  sprite: Sprite
+  children?: React.ReactNode
   model: string
 }
 
-const BossArea: FC<Props> = ({ sprite, model }) => {
+const BossArea: FC<Props> = ({ children, model }) => {
   return (
     <>
       <div className='relative h-full'>
         <Scene cameraPosition={[0, 0.3, 0.6]}>
           <UiModel src={model} />
         </Scene>
-        <div className='absolute top-10 flex justify-center w-full text-slate-300'>
-          <div className='w-3/4'>
-            <div>Lv.{sprite.level} {sprite.name}</div>
-            <HpBar hp={sprite.hp} maxHp={sprite.maxHp} />
-          </div>
-        </div>
+        {children}
       </div>
     </>
   )
--- src/app/orb-match/battle/page.tsx
+++ src/app/orb-match/battle/page.tsx
@@ -6,6 +6,9 @@ import { getUserInfo } from '@/api/module/orb-match'
 import { sprites } from '@/app/common/configs/sprites'
 import { STAGE_COUNT } from '../tunnel/constants/stage'
 import { getSpriteByKey, Sprite } from '../core/sprites'
+import HpBar from '@/app/common/components/ui/hpBar'
+import { Challenger } from '../core/challenger'
+import { getWeaponById } from '../core/weapons'
 
 interface Props { }
 
@@ -22,11 +25,38 @@ const OrbMatchBattle: FC<Props> = () => {
     setSprite(getSpriteByKey(spriteConfig.key, stage))
   }, [spriteConfig])
 
+  const [challenger, setChallenger] = useState<Challenger>()
+  const { data: userConfig } = useFetch(getUserInfo, [])
+  useEffect(() => {
+    if (!userConfig) { return }
+
+    const challenger = new Challenger(
+      userConfig.weapon ? getWeaponById(userConfig.weapon) : null,
+      userConfig.blessings
+    )
+    setChallenger(challenger)
+  }, [userConfig])
+
   return (
     <>
       <div className='relative flex w-screen h-screen'>
         <div className='w-1/2'>
-          {sprite && <BossArea sprite={sprite} model={spriteConfig.model} />}
+          {sprite && <BossArea model={spriteConfig.model}>
+            <div className='absolute top-10 flex justify-center w-full text-slate-300'>
+              <div className='w-3/4'>
+                <div>Lv.{sprite.level} {sprite.name}</div>
+                <HpBar hp={sprite.hp} maxHp={sprite.maxHp} />
+              </div>
+            </div>
+            <div className='absolute bottom-10 lg:bottom-20 flex justify-center w-full text-slate-300'>
+              {
+                challenger && <div className='w-3/4'>
+                  <div className='text-right'>挑戰者</div>
+                  <HpBar hp={challenger.hp} maxHp={challenger.maxHp} />
+                </div>
+              }
+            </div>
+          </BossArea>}
         </div>
         <div className='absolute right-0 pe-3 h-full min-w-[50%] flex items-center'>
           <OrbMatch />
