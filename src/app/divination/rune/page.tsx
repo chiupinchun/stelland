62a6ae41d99bb49c5ef3e3cac388451701e86097
Hato <fansintuesan@gmail.com>
2024-12-22 19:59:54 +0800

    feat: add article

--- src/app/divination/rune/page.tsx
+++ src/app/divination/rune/page.tsx
@@ -9,17 +9,26 @@ const RuneDivination: FC<Props> = () => {
 
   return (
     <>
-      <main className='flex justify-center items-center h-screen'>
-        <div className='grid grid-cols-3 gap-5 w-fit'>
-          {runes.map(rune => (
-            <Rune
-              key={rune.key}
-              isActive={selectedRune === rune}
-              onClick={() => setSelectedRune(rune)}
-            >
-              <img src={rune.icon} width={75} height={75} />
-            </Rune>
-          ))}
+      <main className='flex flex-col justify-center items-center h-screen'>
+        <div>
+          <div className='mb-3 md:mb-8 text-start text-white'>
+            <h2 className='mb-2 font-bold text-lg'>符文占卜</h2>
+            <p className='text-slate-300'>
+              請在心中默念你的問題，<br />
+              並憑直覺選擇一顆符文。
+            </p>
+          </div>
+          <div className='grid grid-cols-2 md:grid-cols-3 gap-5 w-fit'>
+            {runes.map(rune => (
+              <Rune
+                key={rune.key}
+                isActive={selectedRune === rune}
+                onClick={() => setSelectedRune(rune)}
+              >
+                <img src={rune.icon} width={75} height={75} />
+              </Rune>
+            ))}
+          </div>
         </div>
       </main>
     </>
