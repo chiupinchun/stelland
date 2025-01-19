fed7b3d109abf768ebe3604db777d5b4e35a96e9
Hato <fansintuesan@gmail.com>
2025-01-19 12:05:16 +0800

    style: add fixed bg

--- src/app/diary/page.tsx
+++ src/app/diary/page.tsx
@@ -4,6 +4,7 @@ import { twMerge } from 'tailwind-merge'
 import Card from '../common/components/ui/card'
 import { initialState, reducer } from './reducer'
 import Avatar from '../common/components/ui/avatar'
+import bg from '@/assets/images/spirits.webp'
 
 interface Props { }
 
@@ -12,64 +13,68 @@ const Diary: FC<Props> = () => {
 
   return (
     <>
-      <main className='py-5 container text-slate-700'>
-        <ul className='grid grid-cols-2 md:grid-cols-3 gap-5'>
-          {sprites.map(sprite => (
-            <li key={sprite.key} className='flex justify-center'>
-              <a
-                onClick={() => dispatch({
-                  type: 'set_author',
-                  payload: sprite.key
-                })}
-                className={twMerge(
-                  'px-4 py-2 bg-slate-300 rounded-xl cursor-pointer hover:bg-slate-200',
-                  state.author === sprite.key ? 'shadow-md shadow-blue-300 font-bold text-blue-700' : ''
-                )}
-              >
-                {sprite.name}
-              </a>
-            </li >
-          ))}
-        </ul>
-
-        <hr className='my-5' />
-
-        <section className='space-y-5'>
-          {state.diaries.map((diary, index) => (
-            <Card key={index}>
-              <div className='flex items-center gap-5 mb-5'>
-                <Avatar src={spriteMap[state.author].avatar} />
-                <h3 className='font-bold text-lg'>{diary.title}</h3>
-              </div>
-              <p className={twMerge(
-                diary.collapsed ? 'truncate ...' : 'whitespace-pre-line'
-              )}>{diary.content}</p>
-
-              <ul className={twMerge(
-                'space-y-2 rounded overflow-y-hidden transition-all',
-                diary.collapsed ? 'max-h-0' : 'mt-4 p-3 border max-h-[unset]'
-              )}>
-                {diary.comments.map((comment, index) => (<Fragment key={index}>
-                  {index > 0 && <hr />}
-                  <li className='flex items-start gap-5'>
-                    <Avatar src={spriteMap[comment.author].avatar} width={50} height={50} />
-                    <p className='whitespace-pre-line'>{comment.content}</p>
-                  </li>
-                </Fragment>))}
-              </ul>
-
-              <div className='flex justify-end'>
-                {diary.collapsed && <a
-                  className='text-blue-700 cursor-pointer'
+      <main className='min-h-screen bg-fixed bg-cover' style={{
+        backgroundImage: `url(${bg})`
+      }}>
+        <div className='py-5 container text-slate-700'>
+          <ul className='grid grid-cols-2 md:grid-cols-3 gap-5'>
+            {sprites.map(sprite => (
+              <li key={sprite.key} className='flex justify-center'>
+                <a
                   onClick={() => dispatch({
-                    type: 'toggle_collapse',
-                    payload: diary
+                    type: 'set_author',
+                    payload: sprite.key
                   })}
-                >展開</a>}
-              </div>
-            </Card>
-          ))}
-        </section>
+                  className={twMerge(
+                    'px-4 py-2 bg-slate-300 rounded-xl cursor-pointer hover:bg-slate-200',
+                    state.author === sprite.key ? 'shadow-md shadow-blue-300 font-bold text-blue-700' : ''
+                  )}
+                >
+                  {sprite.name}
+                </a>
+              </li >
+            ))}
+          </ul>
+
+          <hr className='my-5' />
+
+          <section className='space-y-5'>
+            {state.diaries.map((diary, index) => (
+              <Card key={index}>
+                <div className='flex items-center gap-5 mb-5'>
+                  <Avatar src={spriteMap[state.author].avatar} />
+                  <h3 className='font-bold text-lg'>{diary.title}</h3>
+                </div>
+                <p className={twMerge(
+                  diary.collapsed ? 'truncate ...' : 'whitespace-pre-line'
+                )}>{diary.content}</p>
+
+                <ul className={twMerge(
+                  'space-y-2 rounded overflow-y-hidden transition-all',
+                  diary.collapsed ? 'max-h-0' : 'mt-4 p-3 border max-h-[unset]'
+                )}>
+                  {diary.comments.map((comment, index) => (<Fragment key={index}>
+                    {index > 0 && <hr />}
+                    <li className='flex items-start gap-5'>
+                      <Avatar src={spriteMap[comment.author].avatar} width={50} height={50} />
+                      <p className='whitespace-pre-line'>{comment.content}</p>
+                    </li>
+                  </Fragment>))}
+                </ul>
+
+                <div className='flex justify-end'>
+                  {diary.collapsed && <a
+                    className='text-blue-700 cursor-pointer'
+                    onClick={() => dispatch({
+                      type: 'toggle_collapse',
+                      payload: diary
+                    })}
+                  >展開</a>}
+                </div>
+              </Card>
+            ))}
+          </section>
+        </div>
       </main>
     </>
   )
